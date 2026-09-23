2fc8548f937a7831dd19d95c92ccadf6c7600b38
Cecile Compte <cecompte@bess-f4r4s7.clusters.42paris.fr>
2026-09-23 12:55:26 +0200

    v0.3 mariadb script

--- srcs/requirements/mariadb/tools/init-db.sh
+++ srcs/requirements/mariadb/tools/init-db.sh
@@ -1,3 +1,19 @@
-if [ ! -d "/var/lib/mysql/wordpress" ]; then
-	echo "hello"
-fi
+#!/bin/sh
+
+# exits immediately when a command fails
+set -e
+
+service mysql start;
+
+mysql -u root << EOF
+CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;
+CREATE USER IF NOT EXISTS '$MYSQL_USER'@'$DOMAIN_NAME' IDENTIFIED BY '$MYSQL_PASSWORD';
+GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'$DOMAIN_NAME';
+FLUSH PRIVILEGES;
+EOF
+
+# shutdown and restart
+mysqladmin -u root shutdown
+
+#starts mysqld_safe
+exec "$@"
