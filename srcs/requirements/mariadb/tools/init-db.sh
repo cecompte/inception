4a9c4b214279945a26cb9ff09155f9bf54a2f39a
Cecile Compte <cecompte@bess-f4r4s7.clusters.42paris.fr>
2026-09-24 13:34:26 +0200

    v0.3.1 finish mariadb script + useful commands

--- srcs/requirements/mariadb/tools/init-db.sh
+++ srcs/requirements/mariadb/tools/init-db.sh
@@ -3,7 +3,14 @@
 # exits immediately when a command fails
 set -e
 
-service mysql start;
+#starts mariadb server in the background (without network connections)
+mysqld_safe --skip-networking &
+mysql_pid=$!
+
+#waits until mariadb is ready (server accepts connections)
+until mysqladmin ping --silent; do
+    sleep 1
+done
 
 mysql -u root << EOF
 CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;
@@ -12,8 +19,8 @@ GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'$DOMAIN_NAME';
 FLUSH PRIVILEGES;
 EOF
 
-# shutdown and restart
+# shuts down temporary server
 mysqladmin -u root shutdown
+wait "$mysql_pid"
 
-#starts mysqld_safe
-exec "$@"
+exec mysqld_safe
